3ec6c33513481e50522c878c9b0990efe8210dd1
shturnev <sht_job@ukr.net>
2016-10-17 01:15:50 +0300

    Создал таблицу task

---
+++ quickstart/app/Task.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App;
+
+use App\User;
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    /**
+     * Массово присваиваемые атрибуты.
+     *
+     * @var array
+     */
+    protected $fillable = ['name'];
+
+
+    /**
+     * Получить пользователя - владельца данной задачи
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
