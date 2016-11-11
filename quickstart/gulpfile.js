219f6187e93c17dec4993918e049e8dec3c763fb
shturnev <sht_job@ukr.net>
2016-11-12 00:07:31 +0200

    +

--- quickstart/gulpfile.js
+++ quickstart/gulpfile.js
@@ -15,5 +15,8 @@ require('laravel-elixir-vue-2');
 
 elixir(mix => {
     mix.sass('app.scss')
-       .webpack('app.js');
+       .webpack('app.js')
+    .copy('node_modules/bootstrap-sass/assets/fonts/bootstrap/',
+        'public/fonts/bootstrap');
+
 });
--- quickstart/resources/views/layouts/css.blade.php
+++ quickstart/resources/views/layouts/css.blade.php
@@ -5,24 +5,6 @@
 <link rel="stylesheet" href="https://use.fontawesome.com/8551de4ebd.css">
 
 <style>
-    body{
-        padding-top: 65px;
-        background-color: white;
-    }
 
-    .circ{
-        width : 40px;
-        vertical-align: middle;
-        border-radius: 50%;
-        box-shadow: 0 0 0 2px #fff;
-        margin-top: 5px;
-    }
-    .circ:hover{
-        box-shadow: 0 0 0 3px #f00;
-    }
-
-    .carousel-inner img {
-        margin: auto;
-    }
     
 </style>
